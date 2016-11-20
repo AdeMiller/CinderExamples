6bff5c8d336943d5ff2b8b1bf9344eee2e388bc2
Ade Miller <ade@ademiller.com>
2016-11-19 16:45:45 -0800

    Sudoku: Initial commit. Load puzzle and draw basic screen and simple solver

---
+++ Sudoku/src/Solver.h
@@ -0,0 +1,142 @@
+//
+// Created by ade on 11/17/16.
+//
+
+#ifndef SUDOKU_SOLVER_H
+#define SUDOKU_SOLVER_H
+
+#include <numeric>
+#include <cctype>
+#include <strings.h>
+
+using namespace ::std;
+
+struct cell {
+    unsigned char val;
+    bool locked;
+    unsigned short mask;
+
+    cell() : val(0), locked(false), mask(0b0000000111111111) {}
+
+    cell(unsigned char v) : val(v), locked(v != 0), mask(0b0000000111111111)
+    {
+        if (v)
+            mask = 0b0000000111111111 & (0b0000001000000000 >> (10 - v));
+    }
+};
+
+struct row_indexer {
+    row_indexer(const int& row) : i(row * 9) {}
+    int operator()() { return i++; }
+
+private:
+    int i;
+};
+
+struct column_indexer {
+    column_indexer(const int& col) : i(col - 9) {}
+    int operator()() { i += 9; return i; }
+
+private:
+    int i;
+};
+
+struct block_indexer {
+    block_indexer(const int& block) : row((block / 3) * 3), col((block % 3) * 3), i(-1) {}
+    int operator()() { ++i; return ((row + i / 3)* 9 + col + i % 3); }
+
+private:
+    int i;
+    int row;
+    int col;
+};
+
+struct read_sdm {
+    cell operator()(char c)
+    {
+        return (isdigit(c)) ? cell(atoi(&c)) : cell();
+    }
+};
+
+class SudokuSolver {
+
+private:
+    array<cell, 81> board;
+    array<array<char, 9>, 27> groups;
+
+public:
+    SudokuSolver() : board() {
+        for (auto i = 0; i < 9; ++i)
+        {
+            generate(begin(groups[i]), end(groups[i]), row_indexer(i));
+            generate(begin(groups[9 + i]), end(groups[9 + i]), column_indexer(i));
+            generate(begin(groups[18 + i]), end(groups[18 + i]), block_indexer(i));
+        }
+    }
+
+    cell get_cell(int row, int col) const { return board[row * 9 + col]; }
+
+    void load_sdm(string data)
+    {
+        if (data.length() != 81) return;
+        transform(cbegin(data), cend(data), begin(board), read_sdm());
+    }
+
+    bool update()
+    {
+        bool changed = false;
+        for (auto g: groups)
+        {
+            unordered_map<unsigned short, int> count(9);
+            auto m = 0b0;
+            for (auto i: g)
+            {
+                if (board[i].val != 0)          // Add known values to masks
+                    m |= board[i].mask;
+                else
+                    count[board[i].mask]++;     // Count populations of masks
+            }
+
+            for (auto i: g)                     // Remove known values from group
+                if (board[i].val == 0)
+                    board[i].mask &= ~m;
+
+            for (auto c: count)
+                if (__builtin_popcount(c.first) > 1 && __builtin_popcount(c.first) == c.second)
+                    for (auto i: g)
+                        if (board[i].val == 0 && board[i].mask != c.first)
+                            board[i].mask &= ~c.first;
+        }
+
+        for (auto& c: board)
+        {
+            if ((c.val == 0) && (__builtin_popcount(c.mask) == 1))
+            {
+                c.val = ffs(c.mask);
+                changed = true;
+            }
+        }
+
+        return changed;
+    }
+
+    bool is_complete()
+    {
+        return all_of(begin(board), end(board), [](const cell c) { return c.val != 0; }) && is_correct();
+    }
+
+    bool is_correct()
+    {
+        for (auto g: groups)
+        {
+            auto count = 0b0;
+            for (auto i: g)
+                count += 0b1 << board[i].val;
+            if (count != 0b0000001111111110)
+                return false;
+        }
+        return true;
+    }
+};
+
+#endif //SUDOKU_SOLVER_H
---
+++ Sudoku/src/SudokuApp.cpp
@@ -0,0 +1,133 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Utilities.h"
+
+#include "Solver.h"
+
+using namespace ci;
+using namespace ci::app;
+
+class SudokuApp : public App {
+
+public:
+    void setup() override;
+
+    void keyDown(KeyEvent event) override;
+    void resize() override;
+    void draw() override;
+    void update() override;
+
+private:
+    SudokuSolver m_solver;
+    Font big_font;
+    Font sml_font;
+    bool is_dirty;
+    bool is_complete;
+};
+
+void prepareSettings(SudokuApp::Settings* settings)
+{
+    settings->setMultiTouchEnabled( false );
+}
+
+void SudokuApp::keyDown(KeyEvent event)
+{
+    is_dirty = m_solver.update();
+    if (!is_dirty)
+        is_complete = m_solver.is_complete();
+}
+
+
+void SudokuApp::resize()
+{
+    is_dirty = true;
+}
+
+void SudokuApp::setup()
+{
+    big_font = Font( "Courier New", 42.0f );
+    sml_font = Font( "Courier New", 16.0f );
+    setWindowSize(100 + 90 * 9, 100 + 90 * 9);
+
+    gl::clear(ColorA::black());
+
+    // NO m_solver.load_sdm("016400000200009000400000062070230100100000003003087040960000005000800007000006820");
+    m_solver.load_sdm("........74.6..7.....71285.6..3.71.5.8.......3.1.84.2..6.89327.....4..9.51........");
+    //m_solver.load_sdm("964.........6..1......7.5...8.9.3...25......63...4...7.....4.....25...4.6..8....3");
+    //m_solver.load_sdm("9672415832.......64.......98.......57958321641.......26.......75.......1321756498");
+    //m_solver.load_sdm("97.....5.6..5.8.3....6..748...3...2..6.....9..1...9...187..5....3.2.7..4.2.....73");
+    //m_solver.load_sdm("97....4.8...1....5....54.....98....414.....763....19.....67....4....9...7.6....91");
+    //m_solver.load_sdm("97...483..5..8......631...........2.7..925..8.4...........672......4..9..351...76");
+    //m_solver.load_sdm("97...6.....4..8...1.2...6..3...9...5428...31.7...3...6..6...7.1...2..4.....1...58");
+    //m_solver.load_sdm("97...6.5...67..21.....5...668......7..5...9..7......414...7.....37..26...2.5...73");
+    //m_solver.load_sdm("97..581.....9...2..2...6.5.7...82.6.....7.....1.39...2.4.8...1..5...3.....156..79");
+    //m_solver.load_sdm("97.1..4.2...7...9.......761....6.1...85...94...7.9....893.......1...5...7.4..9.36");
+
+    is_dirty = true;
+}
+
+void SudokuApp::update()
+{
+    //is_dirty = m_solver.update();
+}
+
+void SudokuApp::draw()
+{
+    if (!is_dirty)
+        return;
+
+    auto board_offset = ivec2(50.0f, 50.0f);
+    auto sqr_size = 90.0f;
+
+    auto num_size = sqr_size / 3.0f;
+    auto num_mid = num_size / 2.0f;
+
+    auto blk_size = num_size * 3.0f;
+    auto blk_mid = blk_size / 2.0f;
+
+    for (auto x = 0; x <= 9; ++x)
+        gl::drawLine(board_offset + ivec2(x * sqr_size, 0.0f), board_offset + ivec2(x * sqr_size, sqr_size * 9.0f));
+        for (auto y = 0; y <= 9; ++y)
+            gl::drawLine(board_offset + ivec2(0.0f, y * sqr_size), board_offset + ivec2(sqr_size * 9.0f, y * sqr_size));
+
+    for (auto x = 0; x < 9; ++x) {
+        gl::drawLine(board_offset + ivec2(x * sqr_size, 0.0f), board_offset + ivec2(x * sqr_size, sqr_size * 9.0f));
+        for (auto y = 0; y < 9; ++y) {
+            gl::color(ColorA::black());
+
+            auto co = board_offset + ivec2(2 + x * sqr_size, 2 + y * sqr_size);
+            gl::drawSolidRect(Rectf(co, co + ivec2(sqr_size - 4, sqr_size - 4)));
+            gl::color(ColorA::white());
+
+            auto c = m_solver.get_cell(x, y);
+            stringstream buf;
+            if (c.val == 0) {
+                auto cell_offset = board_offset + ivec2(sqr_size * x, sqr_size * y);
+                unsigned short m = 0b0000000000000001;
+                for (auto i = 0; i < 9; ++i) {
+                    if (c.mask & m) {
+                        buf << i + 1;
+                        gl::drawStringCentered(buf.str(),
+                                               cell_offset +
+                                               ivec2(num_mid + num_size * (i % 3), num_mid + num_size * (i / 3)),
+                                               ColorA::white(), sml_font);
+                        buf.str("");
+                        buf.clear();
+                    }
+                    m <<= 1;
+                }
+                continue;
+            }
+            buf << int(c.val);
+            gl::drawStringCentered(buf.str(), board_offset + ivec2(blk_mid + sqr_size * x, blk_mid + sqr_size * y),
+                                   c.locked ? ColorA( 1, 1, 1 ) : (is_complete ? ColorA( 0, 1, 0 ) : ColorA( 1, 0, 0 )),
+                                   big_font);
+            buf.str("");
+            buf.clear();
+        }
+        is_dirty = false;
+    }
+}
+
+CINDER_APP( SudokuApp, RendererGl, prepareSettings )
